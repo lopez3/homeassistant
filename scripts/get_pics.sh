3b1828ad60b1b07b1fc2d4fa8d6d3034f49a00dc
Kris Olesiejuk <kolesiejuk@gmail.com>
2017-02-16 07:52:08 +0100

    Initial commit

---
+++ scripts/get_pics.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "${0%/*}"
+
+#/usr/bin/avconv -v quiet -i rtsp://user:pass@192.168.88.100:554/Streaming/Channels/2 -an -frames:v 2 -c:v mjpeg  -q:v 2 -f image2pipe - > salon_tmp.jpg 
+#/usr/bin/avconv -v quiet -i rtsp://user:pass@192.168.88.101:554/Streaming/Channels/2 -an -frames:v 2 -c:v mjpeg  -q:v 2 -f image2pipe - > drzwi_tmp.jpg 
+
+#/usr/bin/avconv -v quiet -i rtsp://user:pass@192.168.88.100:554/Streaming/Channels/1 -an -frames:v 1 -c:v mjpeg  -f image2pipe - > salon_HD.jpg 
+#/usr/bin/avconv -v quiet -i rtsp://user:pass@192.168.88.101:554/Streaming/Channels/1 -an -frames:v 1 -c:v mjpeg  -f image2pipe - > drzwi_HD.jpg 
+
+#wget -q -O salon_tmp.jpg --user=user --password=pass http://192.168.88.100/Streaming/Channels/1/picture 
+#wget -q -O drzwi_tmp.jpg --user=user --password=pass http://192.168.88.101/Streaming/Channels/1/picture 
+
+WGETRC=/home/homeassistant/.homeassistant/scripts/.wgetrc wget -q -O salon_tmp.jpg http://192.168.88.100/Streaming/Channels/1/picture &
+WGETRC=/home/homeassistant/.homeassistant/scripts/.wgetrc wget -q -O drzwi_tmp.jpg http://192.168.88.101/Streaming/Channels/1/picture &
+WGETRC=/home/homeassistant/.homeassistant/scripts/.wgetrc wget -q -O brama_tmp.jpg http://192.168.88.103/Streaming/Channels/1/picture &
+wait
+
+mv -f salon_tmp.jpg /home/homeassistant/.homeassistant/www/local/salon.jpg
+mv -f drzwi_tmp.jpg /home/homeassistant/.homeassistant/www/local/drzwi.jpg
+mv -f brama_tmp.jpg /home/homeassistant/.homeassistant/www/local/brama.jpg
+
+#wget --user=user --password=pass http://192.168.88.102:8080/stream/snapshot.jpg
